79af7654cc3f8b9a685ac2cb7d759f1d5835f08c
Nacho <Nacho@192.168.1.198>
2016-10-17 17:33:29 +0000

    agragar comentarios Javadoc

--- src/dam_ed04_actividad/CCuenta.java
+++ src/dam_ed04_actividad/CCuenta.java
@@ -6,56 +6,89 @@
 /**
  * Tarea para ED04 José Ignacio Navarro Arranz
  */
+
 /**
  * @author  José Ignacio Navarro
  */
 
 package dam_ed04_actividad;
 
-
+/**
+ *
+ * @author Nacho
+ */
 public class CCuenta {
 
-
+/**
+ * Atributos de la clase CCuenta
+ */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-
+/**
+ * Constructor
+ */
     public CCuenta()
     {
     }
-//Incliuimos el siguiente comentario para realizar un nuevo commit
+    
+/**
+ * Incliuimos el siguiente comentario para realizar un nuevo commit
+ * @param nom
+ * @param cue
+ * @param sal
+ * @param tipo 
+ */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+/**
+ * Método para asignar nombre
+ * @param nom 
+ */
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
-
+/**
+ * Método para obtener nombre
+ * @return 
+ */
     public String obtenerNombre()
     {
         return getNombre();
     }
-
+/**
+ * Método para obtener el estado de la cuenta
+ * @return 
+ */
 
     public double estado()
     {
         return getSaldo();
     }
 
-
+/**
+ * Método para ingrear una cantidad
+ * @param cantidad
+ * @throws Exception 
+ */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
+    /**
+     * Método para retirar una cantidad
+     * @param cantidad
+     * @throws Exception 
+     */
 
     public void retirar(double cantidad) throws Exception
     {
@@ -65,6 +98,11 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
+    
+    /**
+     * Método para obtener número cuenta
+     * @return 
+     */
 
 
     public String obtenerCuenta()
@@ -72,37 +110,69 @@ public class CCuenta {
         return getCuenta();
     }
 
+    /**
+     * Método para obtener el nombre
+     * @return 
+     */
 
     public String getNombre() {
         return nombre;
     }
+    /**
+     * Método para asignar nombre
+     * @param nombre 
+     */
 
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-
+    /**
+     * Método para obtener tipo de interes
+     * @return 
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
+    /**
+     * Método paraasignar tipo de interes
+     * @param tipoInterés 
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
+    
+    /**
+     * Método para obtener cuenta
+     * @return 
+     */
 
     public String getCuenta() {
         return cuenta;
     }
-
+    
+    /**
+     * Método para asignar cuenta
+     * @param cuenta 
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+    /**
+     * Método para obtener saldo
+     * @return 
+     */
     public double getSaldo() {
         return saldo;
     }
+    
+    /**
+     * Método para asignar saldo
+     * @param saldo 
+     */
 
     public void setSaldo(double saldo) {
         this.saldo = saldo;
